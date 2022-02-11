f84eaeb8dec0278515577a696b383050b30cf45a
Emilia Crespo <emilia.crespo.est@tecazuay.edu.ec>
2022-02-11 11:46:28 -0500

    ARCHIVO SQL

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+Insert into productos (descripcion,precio,cantidad) values ('Mesa', 35.50, 1);
+Insert into productos (descripcion,precio,cantidad) values ('Cama', 100.50, 1);
+Insert into productos (descripcion,precio,cantidad) values ('Computador', 56.50, 1);
+Insert into productos (descripcion,precio,cantidad) values ('Parlante', 34.50, 1);
+Insert into productos (descripcion,precio,cantidad) values ('Camara', 15.50, 1);
